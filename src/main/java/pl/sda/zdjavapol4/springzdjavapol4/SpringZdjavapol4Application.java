0f2e51d81d70110f9a39f67dbdc81e0eda1c616c
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2020-11-21 13:30:47 +0100

    [init] - project initialization

---
+++ src/main/java/pl/sda/zdjavapol4/springzdjavapol4/SpringZdjavapol4Application.java
@@ -0,0 +1,13 @@
+package pl.sda.zdjavapol4.springzdjavapol4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZdjavapol4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZdjavapol4Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/zdjavapol4/springzdjavapol4/SpringZdjavapol4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.zdjavapol4.springzdjavapol4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZdjavapol4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
